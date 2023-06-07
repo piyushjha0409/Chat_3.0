85d1cd8f728426fc6073931df82f0b87c717a4ad
piyush <peeyush0409@gmail.com>
2023-06-07 17:05:26 +0530

    laptop: solved the utils problem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
    <>
    <h1>This is the home page of the application</h1>
    <Sidebar  setAccount={setAccount} account={account} />
-
+   {/* <Chat account={account} chatContract={undefined} /> */}
    </> 
   );
 }
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ethers } from 'ethers';
 import useMetamask from '../useMetamask';
 
  interface Props {
@@ -7,8 +8,18 @@ import useMetamask from '../useMetamask';
 }
 const Sidebar =  ({ setAccount, account }: Props) => {
   //using the hook
-    const isMetaMaskInstalled = useMetamask(); //this will give either true or false
+  const isMetaMaskInstalled = useMetamask(); //this will give either true or false
 
+  //function for handling the connection to metamask
+  const handleConnection = () => {
+    window.ethereum
+    .request({
+      method: "eth_requestAccounts"
+    })
+    .then((accounts: string[])=>{
+      setAccount(ethers.getAddress(accounts[0]))
+    }).catch((err: any) => console.log(err))
+  }
   return (
     <div className='sidebar'>
      <h1>This is the sidebar!</h1>
