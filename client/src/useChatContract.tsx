f89886d3316f95de19c13384b8a6858c6a4e5807
piyush <peeyush0409@gmail.com>
2023-06-08 00:52:00 +0530

    laptop: connecting the contract from appliction

--- client/src/useChatContract.tsx
+++ client/src/useChatContract.tsx
@@ -2,10 +2,29 @@ import { ethers } from "ethers";
 import { useState, useEffect } from "react";
 
 
+const useChatContract = (
+    contractAddress: string,
+    web3ChatAbi: ethers.ContractInterface,
+    account ?: string
+): ethers.Contract | undefined => {
 
-const useChatContract = () =>{
+    const [signer , setSigner] = useState<ethers.JsonRpcSigner>();
+    const [webthreeProvider, setWebThreeProvider] = useState<ethers.BrowserProvider>()
 
+    const {ethereum} = window;
 
+    useEffect(() => {
+        if(ethereum){
+            setWebThreeProvider(new ethers.BrowserProvider(window.ethereum))
+        }
+    }, [ethereum])
+
+    useEffect(() => {
+      if(webthreeProvider && account){
+          setSigner(webthreeProvider.getSigner())
+      }
+    })
+return new
 }
 
 export default useChatContract;
